9de1764e1208f1f37e7e972f7bb3f20b0e677f28
esthernzau <esthernzau21@gmail.com>
2026-04-24 06:14:20 +0200

    untrack build directory

--- build/CMakeFiles/DSA_GROUP_20.dir/src/min_heap.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DSA_GROUP_20.dir/src/min_heap.cpp.o: \
- /mnt/c/Users/nthab/OneDrive/Desktop/MSIT-2025/Semester\ 2\ -\ Spring/DSA/Project/dsa_group_20/src/min_heap.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/DSA_GROUP_20.dir/src/trie.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DSA_GROUP_20.dir/src/trie.cpp.o: \
- /mnt/c/Users/nthab/OneDrive/Desktop/MSIT-2025/Semester\ 2\ -\ Spring/DSA/Project/dsa_group_20/src/trie.cpp \
- /usr/include/stdc-predef.h
